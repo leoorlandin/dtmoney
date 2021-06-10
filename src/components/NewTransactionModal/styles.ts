9a402e68ea3978ee122f948bb28febe0f42816a5
Leonardo Orlandin Dornelas <orlandin.leo@gmail.com>
2021-06-10 14:39:06 -0300

    add fake post route to create a transaction

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
-import { darken } from 'polished';
+import { darken, transparentize } from 'polished';
 
-export const Container = styled.div`
+export const Container = styled.form`
   h2 {
     color: var(--text-title);
     font-size: 1.5rem;
@@ -56,7 +56,13 @@ export const TransactionTypeContainer = styled.div`
 `
 
 interface RadioBoxProps {
-  isActive: boolean
+  isActive: boolean,
+  activeColor: 'green' | 'red'
+};
+
+const colors = {
+  green: '#33CC95',
+  red: '#E52E4D'
 };
 
 export const RadioBox = styled.button<RadioBoxProps>`
@@ -64,7 +70,9 @@ export const RadioBox = styled.button<RadioBoxProps>`
     border: 1px solid #d7d7d7;
     border-radius: 0.25rem;
 
-    background: ${(props) => props.isActive ? '#e7e7e7' : 'transparent'};
+    background: ${(props) => props.isActive 
+    ? transparentize(0.9, colors[props.activeColor])
+    : 'transparent'};
 
     display: flex;
     align-items: center;
