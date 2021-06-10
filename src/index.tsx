257a85578e516e609f886b48a15e39f969e6b0dc
Leonardo Orlandin Dornelas <orlandin.leo@gmail.com>
2021-06-10 15:02:11 -0300

    get api results to create TransactionsTable component

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,28 @@ createServer({
   models: {
     transaction: Model
   },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de Website',
+          category: 'Dev',
+          type: 'deposit',
+          amount: 400,
+          createdAt: new Date('2021-06-10 14:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-06-10 14:00:00')
+        }
+      ]
+    })
+  },
   routes() {
     this.namespace = 'api';
 
